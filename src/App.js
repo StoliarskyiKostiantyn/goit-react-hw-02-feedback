b809d651d6f993c4d361064cf1ce70b571d67871
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2021-08-02 21:21:17 +0300

    Добил 2-1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import './App.css';
 import React, { Component } from 'react';
-import FeedbackCounter from './Components/FeedbackCounter/FeedbackCounter';
+import Section from './Components/Section/Section';
+import Statistics from './Components/Statistics/Statistics';
+import FeedbackOptions from './Components/FeedbackOptions/FeedbackOptions';
 
 class App extends Component {
   state = { good: 0, neutral: 0, bad: 0 };
-  handleClick = (e) => {
-    this.setState((prevState) => ({
-      const {name}=e.target
+  handleClick = e => {
+    const { name } = e.target;
+    this.setState(prevState => ({
       [name]: prevState[name] + 1,
+    }));
+  };
+  countTotalFeedback = () => {
+    const stateValues = Object.values(this.state);
+    const total = stateValues.reduce((previous, current) => previous + current);
+    return total;
+  };
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    const percentage = (good / total) * 100;
+
+    if (!total) {
+      return 0;
+    }
+
+    return percentage.toFixed();
   };
   render() {
     return (
       <>
-        <div className="App">
-          <FeedbackCounter></FeedbackCounter>
-        </div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions state={this.state} onClick={this.handleClick}></FeedbackOptions>
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            state={this.state}
+            total={this.countTotalFeedback}
+            positivePercentage={this.countPositiveFeedbackPercentage}
+          ></Statistics>
+        </Section>
       </>
     );
   }
 }
 
 export default App;
-
-//         <Section title="">
-//             <Statistics good={ } neutral={ } bad={ } total={ } positivePercentage={ }></Statistics>
-//             <FeedbackOptions options={ } onLeaveFeedback={ }></FeedbackOptions>
-// </Section>
